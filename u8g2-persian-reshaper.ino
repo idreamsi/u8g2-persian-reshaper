68bbaedfef7a626a1f7c2b0b3840305cbfeaff43
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2019-09-15 10:19:52 +0430

    Update u8g2-persian-reshaper.ino

--- u8g2-persian-reshaper.ino
+++ u8g2-persian-reshaper.ino
@@ -12,8 +12,8 @@
 #include <Wire.h>
 #endif
 
+U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6);	// Arduboy (Production, Kickstarter Edition)
 
-U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /* reset=*/ 6);
 //----------------------------------------------------------------------------
 //#define DEBUG
 #define LCDWidth                        u8g2.getDisplayWidth()

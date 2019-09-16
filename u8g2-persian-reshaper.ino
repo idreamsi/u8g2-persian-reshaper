5e5ff7694eb49c8ad6733673278cba46dd07348b
Infinite Dreams <8011806+idreamsi@users.noreply.github.com>
2019-09-16 10:43:04 +0430

    Update u8g2-persian-reshaper.ino

--- u8g2-persian-reshaper.ino
+++ u8g2-persian-reshaper.ino
@@ -22,6 +22,7 @@ U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 12, /* dc=*/ 4, /*
 #define ALINE_LEFT                      0
 #define N_DISTINCT_CHARACTERS           62
 #define IS_UNICODE(c)                   (((c) & 0xc0) == 0xc0)
+#define VERSION				1
 //----------------------------------------------------------------------------
 typedef struct prGlyph {
   int AsciiCode;
